00262dd1a3a4d28962817dc04ecfdcc9d69d592f
Jeremiah <jeremiah.d.wise@gmail.com>
2022-08-26 10:01:39 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+    <main>
+      <Routes>
+        <Route index element={<Users />}/>
+        <Route path='/places/new' element={<NewPlace />}/>
+        <Route path='*' element={<Users />}/>
+      </Routes>
+
+    </main>
+    </>
+  )
 }
 
 export default App;
